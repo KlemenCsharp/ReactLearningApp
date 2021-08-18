d9f5f2e8db70dfd62c4185356efa3f48e9964fc0
Klemen Zupan <zupanklem@gmail.com>
2021-08-18 21:30:34 +0200

    Added input states

--- src/components/login-layout/LoginForm.js
+++ src/components/login-layout/LoginForm.js
@@ -1,15 +1,36 @@
 import { Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
 
 function LoginForm() {
+
+    const usernameChangeHandler = (event) => {
+        setUserInput({
+                ...userInput,  // Takes current state that we can write over, so password is not lost
+                username: event.target.value
+            });
+    };
+    
+    const passwordChangeHandler = (event) => {
+        setUserInput({
+            ...userInput, 
+            password: event.target.value
+        });
+    }
+
+    const [userInput, setUserInput] = useState({
+        username: '',
+        password: ''
+    })
+
     return (
         <Form>
             <Form.Group className="mb-3" controlId="formUsername">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text"></Form.Control>
+                <Form.Control type="text" onChange={usernameChangeHandler} ></Form.Control>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password"></Form.Control>
+                <Form.Control type="password" onChange={passwordChangeHandler}></Form.Control>
             </Form.Group>
             <Button variant="primary" type="submit">Login</Button>
         </Form>
