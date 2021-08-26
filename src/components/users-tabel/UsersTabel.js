5f9c7f14a2af7afce6e36098d0891a6c21689f42
Klemen Zupan <zupanklem@gmail.com>
2021-08-26 21:59:48 +0200

    Created get users methode

--- src/components/users-tabel/UsersTabel.js
+++ src/components/users-tabel/UsersTabel.js
@@ -1,7 +1,18 @@
 function UsersTabel() {
+    function getUsers() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+            console.log(res);
+            return res.json();
+        }).then(data => {
+            console.log(data)
+        });
+    }
+
     return (
         <div>
             <h1>Users tabel</h1>
+            <button onClick={getUsers}>Get Users</button>
         </div>
     )
 }
