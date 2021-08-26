2f6ad4a0221518a5142fb0ede2edb986bbc23c79
Klemen Zupan <zupanklem@gmail.com>
2021-08-26 06:20:55 +0200

    Basic bootstrap navbar

---
+++ src/components/navbar/Topbar.js
@@ -0,0 +1,21 @@
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Topbar.css";
+import Nav from "react-bootstrap/esm/Nav";
+
+function Topbar() {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Simple react app</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/welcome">Home</Nav.Link>
+          <Nav.Link href="/users">Users</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Topbar;
---
+++ src/pages/users/UsersPage.js
@@ -0,0 +1,9 @@
+function UsersPage() {
+    return (
+        <div>
+            Users page works!
+        </div>
+    )
+}
+
+export default UsersPage;
