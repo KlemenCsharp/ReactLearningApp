162947fa1dce700d55843f6cc23f2c96df34d957
Klemen Zupan <zupanklem@gmail.com>
2021-09-02 22:27:16 +0200

    Created succesful user call

--- src/pages/users/UserPage.js
+++ src/pages/users/UserPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-function UserPage() {
-    let { id } = useParams();
+const UserPage = () => {
+  let { id } = useParams();
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState({ userData: {} });
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await axios(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+
+      setUser({ userData: data });
+      console.log(data);
+    };
+    fetchUser();
+  }, [setUser]);
 
   return (
     <div>
       <h1>Id user is {id}</h1>
+      <p>username: {user.userData.username}</p>
     </div>
   );
-}
+};
 
 export default UserPage;
